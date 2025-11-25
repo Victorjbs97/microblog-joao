d652714bb4dec7f3d13c7c1ca7f3f94af55b5655
Victorjbs97 <Victorjbs97@gmail.com>
2025-11-25 11:18:46 -0300

    Commits dia 18/11

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,9 +1,21 @@
 <?php 
+	require_once "../src/Database/Conecta.php";
 	require_once "../src/Helpers/Utils.php";
+	require_once "../src/Services/NoticiaServico.php";
 	require_once "../src/Services/autenticacaoServicos.php";
-	
     AutenticacaoServicos::exigirLogin();
 	
+	$erro = null;
+	$noticias = [];
+	$noticiaServico = new NoticiaServico();
+
+	try {
+		$noticias = $noticiaServico->buscar();
+		//Utils::dump($noticias);
+	} catch (Throwable $e) {
+		$erro = "Erro ao buscar notícias. <br>".$e->getMessage();
+	}
+
 	require_once "../includes/cabecalho-admin.php";
 ?>
 
@@ -12,6 +24,9 @@
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
 		
 		<h2 class="text-center">Notícias <span class="badge bg-dark">X</span></h2>
+		<?php if ($erro): ?>
+			<p class="alert alert-danger text-center"> <?= $erro ?> </p>
+		<?php endif; ?>
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="noticia-insere.php">
---
+++ src/Services/NoticiaServico.php
@@ -0,0 +1,20 @@
+<?php 
+// src/Services/NoticiaServico.php
+
+class NoticiaServico {
+    private PDO $conexao;
+
+    public function __construct() {
+        $this->conexao = Conecta::getConexao();        
+    }
+
+    // Versão básica (provisória)
+    public function buscar():array {
+                $sql = "SELECT  noticias.id,noticias.titulo,noticias.data,usuarios.nome 
+                AS autor FROM noticias JOIN usuarios
+                ON noticias.usuario_id = usuarios.id                
+                ORDER BY data DESC";
+        $consulta = $this->conexao->query($sql);
+        return $consulta->fetchAll();
+    }
+}
